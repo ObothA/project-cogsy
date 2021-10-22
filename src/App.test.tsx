6df870c52cdf562472d8129a0460bde299d397ce
obotha <khyper234@gmail.com>
2021-10-22 12:24:27 +0300

    Create App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders App correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appContainer = screen.getByRole('main');
+  expect(appContainer).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>You know what? Let's do this!</p>
-      </header>
-    </div>
+    <main>
+      <p>You know what? Let's do this!</p>
+    </main>
   );
 }
 
