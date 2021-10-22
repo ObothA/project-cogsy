589b31ca9ee5ed2fbbbbdd86d5b2358a679ad7da
obotha <khyper234@gmail.com>
2021-10-22 16:01:06 +0300

    Remove redudant text from Cards.

--- src/pages/Cards/Cards.tsx
+++ src/pages/Cards/Cards.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 import './cards.css';
 
 export default function Cards() {
-  return (
-    <section data-testid='cards-section' className='cards-container'>
-      All cards go here
-    </section>
-  );
+  return <section data-testid='cards-section' className='cards-container' />;
 }
