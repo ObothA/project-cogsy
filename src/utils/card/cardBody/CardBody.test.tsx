0d9a82f66322abbf1dfe46cf817cffb609f3f0a6
obotha <khyper234@gmail.com>
2021-10-23 17:05:24 +0300

    Add and test main heading in card body.

--- src/utils/card/cardBody/CardBody.test.tsx
+++ src/utils/card/cardBody/CardBody.test.tsx
@@ -6,6 +6,11 @@ import CardBody from './CardBody';
 test('Card body renders correctly.', () => {
   render(<CardBody />);
   const cardBody = screen.getByTestId('card-body');
-
   expect(cardBody).toBeInTheDocument();
+
+  const cardHeader = screen.getByTestId('card-header');
+  expect(cardHeader).toBeInTheDocument();
+
+  const mainHeading = screen.getByTestId('main-heading');
+  expect(mainHeading).toHaveClass('heading');
 });
--- src/utils/card/cardBody/CardBody.tsx
+++ src/utils/card/cardBody/CardBody.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import './cardBody.css';
+
 export default function CardBody() {
   return (
-    <div data-testid='card-body' className='p-3'>
-      dffd
-    </div>
+    <section data-testid='card-body' className='p-3'>
+      <header data-testid='card-header'>
+        <h6 data-testid='main-heading' className='heading'>
+          Important Announcement - Unl
+        </h6>
+      </header>
+      <section></section>
+      <section></section>
+    </section>
   );
 }
