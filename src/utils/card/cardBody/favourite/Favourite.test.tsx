02d83864a58c64913e42d8b3ef243b5ac6044f2d
obotha <khyper234@gmail.com>
2021-10-25 14:22:13 +0300

    Test and implement on click on inactive Favourite.

--- src/utils/card/cardBody/favourite/Favourite.test.tsx
+++ src/utils/card/cardBody/favourite/Favourite.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Favourite from './Favourite';
 
-test('Favourite renders correctly', () => {
-  render(<Favourite />);
-  const favourite = screen.getByTestId('favourite');
-  expect(favourite).toBeInTheDocument();
+describe('Favourite renders correctly', () => {
+  test('Has the correct class', () => {
+    render(<Favourite />);
+    const favourite = screen.getByTestId('favourite');
+    expect(favourite).toHaveClass('col');
+  });
+
+  test('On click, renders filled star icon.', () => {
+    render(<Favourite />);
+    const iconSpan = screen.getByTestId('star-icon-span');
+    userEvent.click(iconSpan);
+    expect(iconSpan).toHaveClass('star-active-true');
+  });
 });
--- src/utils/card/cardBody/favourite/Favourite.tsx
+++ src/utils/card/cardBody/favourite/Favourite.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { AiOutlineStar } from 'react-icons/ai';
-// import { AiFillStar, AiOutlineStar } from 'react-icons/all';
+import React, { useState } from 'react';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 
+import './favourite.css';
+
 export default function Favourite() {
+  const [active, setActive] = useState(false);
+
   return (
     <div data-testid='favourite' className='col'>
-      <IconContext.Provider value={{ color: '#2a2941', size: '17px' }}>
-        <div>
-          <AiOutlineStar />
-        </div>
+      <IconContext.Provider value={{ color: '#2a2941', size: '18px' }}>
+        <span
+          className={`icon-container star-active-${active}`}
+          onClick={() => setActive(!active)}
+          data-testid='star-icon-span'
+        >
+          {active ? <AiFillStar /> : <AiOutlineStar />}
+        </span>
       </IconContext.Provider>
     </div>
   );
