8f17af9a05aac9f37a537ff283a6ee872dbb19c4
obotha <khyper234@gmail.com>
2021-10-26 11:35:28 +0300

    Remove unnecessary padding.

--- src/utils/card/cardThumbnail/Thumbail.tsx
+++ src/utils/card/cardThumbnail/Thumbail.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import thumbnailImg from '../../../assets/img/pexels-jopwell-small.jpg';
+import mainAsset from '../../../assets/img/pexels-jopwell-original.jpg';
 import './thumbnail.css';
 
 export default function Thumbail() {
-  return <div data-testid='thumbnail' className='thumbnail-container row' />;
+  return (
+    <div data-testid='thumbnail' className='thumbnail-container'>
+      {/* <div className='col'> */}
+      <a href={mainAsset} rel='noreferrer' target='_blank'>
+        {/* <img src={thumbnailImg} alt='thumbnail' className='img-fluid' /> */}
+      </a>
+      {/* </div> */}
+    </div>
+  );
 }
