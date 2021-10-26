b432c2109e700e7b360f0ba4fd00330ccbaaa595
obotha <khyper234@gmail.com>
2021-10-26 11:44:18 +0300

    Add sample thumbnail.

--- src/utils/card/cardThumbnail/Thumbail.tsx
+++ src/utils/card/cardThumbnail/Thumbail.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import thumbnailImg from '../../../assets/img/pexels-jopwell-small.jpg';
 import mainAsset from '../../../assets/img/pexels-jopwell-original.jpg';
-import './thumbnail.css';
 
 export default function Thumbail() {
   return (
-    <div data-testid='thumbnail' className='thumbnail-container'>
-      {/* <div className='col'> */}
+    <div data-testid='thumbnail'>
       <a href={mainAsset} rel='noreferrer' target='_blank'>
-        {/* <img src={thumbnailImg} alt='thumbnail' className='img-fluid' /> */}
+        <img src={thumbnailImg} alt='thumbnail' className='img-fluid' />
       </a>
-      {/* </div> */}
     </div>
   );
 }
