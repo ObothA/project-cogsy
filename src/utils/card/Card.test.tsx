92624629c243e766685ef3f72760b4b0d78aca5d
obotha <khyper234@gmail.com>
2021-10-26 15:03:42 +0300

    Make card responsive.

--- src/utils/card/Card.test.tsx
+++ src/utils/card/Card.test.tsx
@@ -7,5 +7,7 @@ test('Card renders correctly.', () => {
   render(<Card />);
   const cardArticle = screen.getByRole('article');
 
-  expect(cardArticle).toHaveClass('article-card');
+  expect(cardArticle).toHaveClass(
+    'article-card col-xs-12 col-sm-5 col-md-4 col-lg-4 col-xl-3 col-xxl-2'
+  );
 });
--- src/utils/card/Card.tsx
+++ src/utils/card/Card.tsx
@@ -7,7 +7,7 @@ import './card.css';
 
 export default function Card() {
   return (
-    <article className='article-card col'>
+    <article className='article-card col-xs-12 col-sm-5 col-md-4 col-lg-4 col-xl-3 col-xxl-2'>
       <Thumbail />
       <CardBody />
     </article>
